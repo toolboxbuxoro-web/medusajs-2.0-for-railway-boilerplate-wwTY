003618654e03831bd8a618651e5e11cb55bf55f8
toolboxbuxoro-web <toolboxbuxoro@gmail.com>
2025-12-17 16:45:03 +0500

    Do not log secrets from medusa-config

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -212,5 +212,15 @@ const medusaConfig = {
   ]
 };
 
-console.log(JSON.stringify(medusaConfig, null, 2));
+// Avoid logging full config (it may contain secrets).
+console.log("[medusa-config] loaded", {
+  workerMode: WORKER_MODE,
+  disableAdmin: SHOULD_DISABLE_ADMIN,
+  hasRedisUrl: !!REDIS_URL,
+  hasDatabaseUrl: !!DATABASE_URL,
+  hasMeilisearch: !!MEILISEARCH_HOST,
+  hasMinio: !!MINIO_ENDPOINT,
+  hasPayme: !!PAYME_ID,
+  hasClick: !!CLICK_MERCHANT_ID,
+});
 export default defineConfig(medusaConfig);
